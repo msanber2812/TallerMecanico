2c942deb4bac676620f557bb351a7cc547c313f0
Manuel <sanchezberenguelm@gmail.com>
2026-03-10 14:08:48 +0100

    creación de TallerMecanicoExcepcion.java

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/sprint_inicial/TallerMecanicoExcepcion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.tallermecanico.sprint_inicial;
+
+public class TallerMecanicoExcepcion extends Exception {
+    public TallerMecanicoExcepcion(String message) {
+        super(message);
+    }
+}
